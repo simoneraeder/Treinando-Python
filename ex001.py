d6d5ead6322150685d8b19590b8a696193336045
simoneraeder <166766334+simoneraeder@users.noreply.github.com>
2025-07-05 20:27:57 -0300

    python

---
+++ ex001.py
@@ -0,0 +1,3 @@
+print('Hello World')
+nome = 'Simone'
+print(nome)
---
+++ ex003.py
@@ -0,0 +1,3 @@
+n = int (input('Digite um número:'))
+
+print('Analisando o valor {},\n seu antecessor é {} e o sucessor é {}'.format(n,n-1,n+1))
